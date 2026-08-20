f15fd8e436eb0b2099d6f31beea4efedc8a7398f
Uygar Muntas <muntasuygar@gmail.com>
2026-08-20 15:42:23 +0200

    connect backend to postgres with pg pool

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,23 @@
 import express from 'express'
+import 'dotenv/config';
+import { pool } from './db/pool';
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
-app.get('/api/testing', (req,res) => {
+app.get('/api/testing', (_req,res) => {
     res.json({status:'ok'})
 })
+
+app.get('/api/db-test', async (_req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ dbTime: result.rows[0].now });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database connection failed' });
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Backend is running on http://localhost:${PORT}`)
